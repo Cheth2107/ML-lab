018ab5d62e4e2db2cb21ac9260794458b2ae60d3
Chethan K <143086835+Cheth2107@users.noreply.github.com>
2024-03-05 07:39:52 +0530

    Create list.py

---
+++ practice/list.py
@@ -0,0 +1,9 @@
+//Python program to convert list to tuple
+
+def list_to_tuple(input_list):
+    return tuple(input_list)
+
+# Example usage:
+input_list = [1, 2, 3, 4, 5]
+result_tuple = list_to_tuple(input_list)
+print("List converted to tuple:", result_tuple)
