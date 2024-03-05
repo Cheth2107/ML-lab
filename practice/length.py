660a49e9682ccb9834083f499d365d9ef9822fff
Chethan K <143086835+Cheth2107@users.noreply.github.com>
2024-03-05 07:30:43 +0530

    Create length.py

---
+++ practice/length.py
@@ -0,0 +1,9 @@
+//Pyhton program to find tuple length
+
+def find_tuple_length(input_tuple):
+    return len(input_tuple)
+
+# Example usage:
+input_tuple = (1, 2, 3, 4, 5)
+tuple_length = find_tuple_length(input_tuple)
+print("Length of the tuple:", tuple_length)
