2d67ab63cf1ff56dc7199ca4d6d3fe2f4f642564
Chethan K <143086835+Cheth2107@users.noreply.github.com>
2024-03-05 07:27:41 +0530

    Create print.py

---
+++ practice/print.py
@@ -0,0 +1,10 @@
+//Python program to print tuple items
+
+def print_tuple_items(input_tuple):
+    for item in input_tuple:
+        print(item)
+
+# Example usage:
+input_tuple = (1, 2, 3, 4, 5)
+print("Items in the tuple:")
+print_tuple_items(input_tuple)
