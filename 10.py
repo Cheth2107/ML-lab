709127676b90aa503d5dee5cce8fbaaed0688db4
Chethan K <143086835+Cheth2107@users.noreply.github.com>
2024-07-24 09:05:29 +0530

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.decomposition import PCA as SklearnPCA
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns
+
+
+X = load_iris().data
+y = load_iris().target
+
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+
+correlation_matrix = np.corrcoef(X_scaled.T)
+plt.figure(figsize=(6, 6))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f')
+plt.title('Correlation Matrix (After Standardization)')
+plt.show()
+
+
+pca = SklearnPCA(n_components=2)
+X_projected = pca.fit_transform(X_scaled)
+
+print("Shape of Data:", X.shape)
+print("Shape of transformed Data:", X_projected.shape)
+
+
+pc1 = X_projected[:, 0]
+pc2 = X_projected[:, 1]
+
+plt.scatter(pc1, pc2, c=y, cmap="jet")
+plt.xlabel("Principal Component 1")
+plt.ylabel("Principal Component 2")
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns
+
+
+X = load_iris().data
+y = load_iris().target
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+
+correlation_matrix = np.corrcoef(X_scaled.T)
+plt.figure(figsize=(6, 6))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt='.2f')
+plt.title('Correlation Matrix (After Standardization)')
+plt.show()
+
+
+lda = LinearDiscriminantAnalysis(n_components=2)
+X_projected = lda.fit_transform(X_scaled, y)
+
+print("Shape of Data:", X.shape)
+print("Shape of transformed Data:", X_projected.shape)
+
+
+ld1 = X_projected[:, 0]
+ld2 = X_projected[:, 1]
+
+plt.scatter(ld1, ld2, c=y, cmap="jet")
+plt.xlabel("Linear Discriminant 1")
+plt.ylabel("Linear Discriminant 2")
+plt.title("LDA of Iris Dataset (sklearn Implementation)")
+plt.show()
+
+
+plt.title("PCA of Iris Dataset (sklearn Implementation)")
+plt.show()
+
